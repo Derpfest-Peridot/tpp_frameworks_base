d1018beac0479f9d7fa276223e231ffcd5d632e2
Austin T. Conn <atc3030dev@gmail.com>
2020-10-28 11:06:30 -0300

    base: Forward Port Volume Long Press Skip Tracks [1/2]
    Bring Forward Volume Rocker Track Change
    
    Requires Settings Change
    (packages/apps/Settings)
    
    * @brituj : AudioService: Allow volume-key long press even when screen is off
    
    * @mickaelmendes50 : Adapt to Android 11
    
    * Drop previous changes of AudioService as, in theory, the event should
      never reach it at all.
    
    Change-Id: I70b00e08fe48707110725dd1daa56b1e48d15a03
    
    Fix volume rocker skip track on Ambient Display and Lift to Wake
    
    we need to check if dream service is dozing before checking
    keyguard status
    
    Change-Id: Ic3a6c830496188bb6edf27043cd24eb2d553bb82
    
    Fix long press volume buttons skip tracks
    
    Without that patch, the volume changes up or down prior
    to skipping tracks because the sendvolumekeyevent doesn't
    have the mayChangeVolume condition. Also, that part is
    useless cause the mUseTvRouting condition is taken care
    of correctly later.
    
    [aswin7469] Adapted for 14 QPR2
    
    Change-Id: Ic2daa8bcae65e932889d3bccc44c10d11daeb20d
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Co-Authored-By: Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Co-Authored-By: Ido Ben-Hur <idoybh2@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6618,6 +6618,12 @@ public final class Settings {
         public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
 
         /**
+         * Whether or not volume button music controls should be enabled to seek media tracks
+         * @hide
+         */
+        public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
+
+        /**
          * Whether to show advanced reboot options in power menu
          * @hide
          */
